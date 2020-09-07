bd472d7f65385f3cb2a0fc364ac231566dd94b1b
ProgrammerChris <chris.tvedt@gmail.com>
2020-09-07 22:53:48 +0200

    Lagt til landingsside fra Eilert og lagt til JS til knapp for valg av fylke

--- js/index.js
+++ js/index.js
@@ -1,13 +1,32 @@
-// Knapp for å vise riktig ladetid for valgt batterikapasitet
-let knappBeregnladetid = document.getElementById('bergen-ladetid-knapp');
+//Knapp for å velge fylke
+let knappVelgFylke = document.getElementById("valgt-fylke");
+// Hvis knappen blir funnet på siden
+if (knappVelgFylke) {
+    let velgFylke = () => {
+        knappVelgFylke.addEventListener("click", () => {
+            if (document.getElementById("ladepunkt").value == 'Rogaland') {
+                window.location = './rogaland.html';
+            } else if (document.getElementById("ladepunkt").value == 'Vestland') {
+                window.location = './vestland.html';
+            }
+        })
+    }
 
-// tar ibruk riktig verdi i dropdown og viser riktig tabell og graf
-let beregnLadetid = () => {
-    knappBeregnladetid.addEventListener("click", () => {
-        console.log(document.getElementById('kwh').value);
-        document.getElementById('graf-og-tabell').style.display = "block";
-    });
+    knappVelgFylke.onclick = velgFylke();
 }
 
-// bruker onclick metoden for å vise riktig ladetid for valgt batterikapasitet
-knappBeregnladetid.onclick = beregnLadetid();
+
+// Knapp for å vise riktig ladetid for valgt batterikapasitet
+let knappBeregnladetid = document.getElementById("bergen-ladetid-knapp");
+// Hvis knappen blir funnet på siden
+if (knappBeregnladetid) {
+    // tar ibruk riktig verdi i dropdown og viser riktig tabell og graf
+    let beregnLadetid = () => {
+        knappBeregnladetid.addEventListener("click", () => {
+            document.getElementById("graf-og-tabell").style.display = "block";
+        });
+    }
+
+    // bruker onclick metoden for å vise riktig ladetid for valgt batterikapasitet
+    knappBeregnladetid.onclick = beregnLadetid();
+}
